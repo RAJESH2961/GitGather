50270047e4f7caffabb0b203e16a56125af09ed6
Rajesh <grajesh2906@gmail.com>
2025-05-27 12:55:10 +0530

    Mondo db connection

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,21 @@
 //  /api/mew-meetup
-
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+async function handler(req, res) {
     if(req.method === 'POST') {
         const data = req.body;
-        const { title, image, address, description } = data;
-
+        // const { title, image, address, description } = data;
+            // #gBGBRM72931
         // storing data in database
+        const client = MongoClient.connect('mongodb+srv://RAJESH:#gBGBRM72931@nextjs.1aijdyj.mongodb.net/?retryWrites=true&w=majority&appName=nextjs');
+        const db = (await client).db();
+
+        const meetupsCollection = db.collection('meetups');
+        const result = await meetupsCollection.insertOne(data);
+        console.log(result);
+
+        client.close();
+
+        res.status(201).json({message: 'Meetup inserted!'});
     }
 }
 
